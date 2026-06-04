932e818b8048be47b0a2e6c6a5eb1630e94fb01d
Biyse <mrs.busecici@gmail.com>
2026-06-04 22:31:37 +0300

    Update weighted_buse_cici.py

--- Week02/weighted_buse_cici.py
+++ Week02/weighted_buse_cici.py
@@ -1,9 +1,8 @@
 import random
-def weighted_srs(data, n, weights, with_replacement=False):
-    if weights or with_replacement: return random.choices(data, weights=weights, k=n)
-    sample, re_data = [], list(data)
-    for _ in range(n):
-        choisen = random.choices(re_data)[0]
-        sample.append(choisen)
-        re_data.remove(choisen)
-    return sample
+def weighted_srs(data, n, weights, with_replacement = False):
+    if with_replacement :
+        return random.choices(data, weights = weights, k = n)
+    if weights is None :
+        return random.sample(data, n)
+    else :
+        return random.sample(data, n, counts = weights)
